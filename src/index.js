894fb9687034d76735de8d56179628489804668a
Petro Psekho <psekho.pp@gmail.com>
2022-12-20 16:58:41 +0200

    task in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+function fetchCountries(name) {
+  fetch(
+    'https://jsonplaceholder.https://restcountries.com/v3.1/name/peru.com/users'
+  ).then(responcs => response.json());
+}
