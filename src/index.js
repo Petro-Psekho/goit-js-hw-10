beeac4785b72d09aa00ad7526a7b00512ff2e10e
Petro Psekho <psekho.pp@gmail.com>
2022-12-20 22:05:17 +0200

    task  in progress

--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
-function fetchCountries(name) {
+const refs = {
+  inputField: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+console.log(refs.inputField);
+console.log(refs.countryList);
+console.log(refs.countryInfo);
+
+refs.inputField.addEventListener(
+  'input',
+  debounce(fetchCountries, DEBOUNCE_DELAY)
+);
+
+function inputFieldText() {
+  console.log(refs.inputField.value);
+
+  refs.countryList.insertAdjacentHTML(
+    'beforeend',
+    // galleryItems.map(galleryItem => `<li>1111</li>`).join('')
+    `<li>{{capital}}</li>
+    <li>${refs.inputField.value}</li>`
+  );
+}
+
+function fetchCountries() {
   fetch(
-    'https://jsonplaceholder.https://restcountries.com/v3.1/name/peru.com/users'
-  ).then(responcs => response.json());
+    'https://restcountries.com/v3.1/name/poland?name.official&capital&population&flags.svg&languages'
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(country => {
+      console.log(country);
+
+      const markup = inputFieldText(country);
+      console.log(markup);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
