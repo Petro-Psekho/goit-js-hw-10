5b4a0cdf612c8525cffc0f5de61242ef263a5b86
Petro Psekho <psekho.pp@gmail.com>
2022-12-21 16:58:07 +0200

    task in progress

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
+// import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -10,40 +10,46 @@ const refs = {
   countryInfo: document.querySelector('.country-info'),
 };
 
-console.log(refs.inputField);
-console.log(refs.countryList);
-console.log(refs.countryInfo);
+// console.log(refs.inputField);
+// console.log(refs.countryList);
+// console.log(refs.countryInfo);
 
 refs.inputField.addEventListener(
   'input',
-  debounce(fetchCountries, DEBOUNCE_DELAY)
+  debounce(inputCountryName, DEBOUNCE_DELAY)
 );
 
-function inputFieldText() {
-  console.log(refs.inputField.value);
+function inputCountryName(e) {
+  e.preventDefault();
 
-  refs.countryList.insertAdjacentHTML(
-    'beforeend',
-    // galleryItems.map(galleryItem => `<li>1111</li>`).join('')
-    `<li>{{capital}}</li>
-    <li>${refs.inputField.value}</li>`
-  );
+  const countryName = refs.inputField.value.trim();
+  // console.log(countryName);
+
+  fetchCountries(countryName)
+    .then(renderCountryList)
+    .catch(error => console.log(error));
 }
 
-function fetchCountries() {
-  fetch(
-    'https://restcountries.com/v3.1/name/poland?name.official&capital&population&flags.svg&languages'
-  )
-    .then(response => {
-      return response.json();
-    })
-    .then(country => {
-      console.log(country);
+function fetchCountries(countryName) {
+  return fetch(
+    'https://restcountries.com/v2/name/sw?fields=name,capital,population,flags,languages'
+    // 'https://restcountries.com/v2/name/${countryName}'
+  ).then(response => {
+    return response.json();
+  });
+}
 
-      const markup = inputFieldText(country);
-      console.log(markup);
+function renderCountryList(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+          <img src="${country.flags.svg}" alt="${country.name}" width="50">
+          <p><b>Country</b>: ${country.name}</p>
+          <p><b>Capital</b>: ${country.capital}</p>
+          <p><b>Population</b>: ${country.population}</p>
+          <p><b>Languages</b>: ${country.languages[0].name}</p>
+        </li>`;
     })
-    .catch(error => {
-      console.log(error);
-    });
+    .join('');
+  refs.countryList.innerHTML = markup;
 }
