a420f1c329686e61c22a49db433b1922b04f6f26
Petro Psekho <psekho.pp@gmail.com>
2022-12-22 16:52:12 +0200

    add renderCountryList &  wrongCountryName alert

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
@@ -23,15 +23,13 @@ function inputCountryName(e) {
   // console.log(countryName);
 
   if (countryName === '') {
-    return (refs.countryList.innerHTML = ''), (refs.countryInfo.innerHTML = '');
+    return (refs.countryList.innerHTML = ''), (refs.countryInfo.innerHTML = ''); // condition to clear the input field and/or remove the created markup
   }
 
-  fetchCountries(countryName)
-    .then(renderCountryList)
-    .catch(error => console.log(error));
+  fetchCountries(countryName).then(renderCountryInfo).catch(wrongCountryName);
 }
 
-function renderCountryList(countries) {
+function renderCountryInfo(countries) {
   // console.log(countries);
   const markup = countries
     .map(country => {
@@ -42,12 +40,28 @@ function renderCountryList(countries) {
 
       return `<li>
           <img src="${country.flags.svg}" alt="${country.name}" width="50">
-          <p><b>Country</b>: ${country.name}</p>
+          <p>${country.name}</p>
           <p><b>Capital</b>: ${country.capital}</p>
           <p><b>Population</b>: ${country.population}</p>
           <p><b>Languages</b>: ${languages}</p>
         </li>`;
     })
     .join('');
+  refs.countryInfo.innerHTML = markup;
+}
+
+function renderCountryList(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+          <img src="${country.flags.svg}" alt="${country.name}" width="50">
+          <p>${country.name}</p>
+        </li>`;
+    })
+    .join('');
   refs.countryList.innerHTML = markup;
+}
+
+function wrongCountryName() {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
 }
